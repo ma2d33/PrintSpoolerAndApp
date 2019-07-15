34f41bcab20ff7c114b5ececbbe6bec7d695a635
Janis <ma2d33@yandex.ru>
2019-07-15 13:43:59 +0300

    new updates too long to write that shit

--- PrintSpoolerAndApp/Program.cs
+++ PrintSpoolerAndApp/Program.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace PrintSpoolerAndApp
 {
@@ -13,24 +15,60 @@ namespace PrintSpoolerAndApp
         {
             Console.Clear();
 
-            Printer prt = new Printer();
-            List<string> printers = prt.GetPrintersList();
-            short Index = 0;
+            //Printer prt = new Printer();
+            //List<string> printers = prt.GetPrintersList();
+            //short Index = 0;
 
-            foreach (string printer in printers)
+            
+;
+            // Create a local print server
+            LocalPrintServer ps = new LocalPrintServer();
+
+            // List the print server's queues
+            PrintQueueCollection myPrintQueues = ps.GetPrintQueues();
+           
+            String printQueueNames = "My Print Queues:\n\n";
+
+
+            foreach (PrintQueue pq in myPrintQueues)
             {
-                Console.WriteLine(Index.ToString() + " = " + printer);
-                Index++;
+                pq.Refresh();
+                printQueueNames += "\t" + pq.Name + "\n";
+
+                PrintJobInfoCollection jobs = pq.GetPrintJobInfoCollection();
+
+                bool hasJobs = jobs.Any();
+
+                
+
+                foreach (PrintSystemJobInfo job in jobs)
+                {
+                    // Since the user may not be able to articulate which job is problematic,
+                    // present information about each job the user has submitted.
+                    
+                }
             }
+                Console.WriteLine(printQueueNames);
+                // Console.WriteLine(PrintSystemJobInfo.Get(myPrintQueues,1));
+                Console.WriteLine("\nPress Return to continue.");
 
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("Enter Printer Index:");
-            short printerIndex = Convert.ToInt16(Console.ReadLine());
-            string selectedPrinter = printers[printerIndex];
 
-            Console.WriteLine(prt.GetPrintJobStatus(selectedPrinter));
+                //foreach (string printer in printers)
+                //{
+                //    Console.WriteLine(Index.ToString() + " = " + printer);
+                //    Index++;
+                //}
 
-            Console.WriteLine("Press any key to exit...");
+                //Console.WriteLine(Environment.NewLine);
+                //Console.WriteLine("Enter Printer Index:");
+                //short printerIndex = Convert.ToInt16(Console.ReadLine());
+                //string selectedPrinter = printers[printerIndex];
+
+                //Console.WriteLine(prt.GetPrintJobStatus(selectedPrinter));
+                //Console.WriteLine();
+
+                //Console.WriteLine("Press any key to exit...");
+            
             Console.ReadLine();
         }
     }
