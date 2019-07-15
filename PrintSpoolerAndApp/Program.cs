f2d3da667391444644abda73e5c9fb5f30f4df17
Janis <ma2d33@yandex.ru>
2019-07-15 13:44:16 +0300

    some stuff

--- PrintSpoolerAndApp/Program.cs
+++ PrintSpoolerAndApp/Program.cs
@@ -1,10 +1,7 @@
-﻿using PrinterLib;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using System.Management;
 using System.Printing;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace PrintSpoolerAndApp
@@ -13,63 +10,38 @@ namespace PrintSpoolerAndApp
     {
         static void Main(string[] args)
         {
-            Console.Clear();
+            bool loopIt = true;
+            string printInfo = "";
+            int jobId = 0;
 
-            //Printer prt = new Printer();
-            //List<string> printers = prt.GetPrintersList();
-            //short Index = 0;
-
-            
-;
-            // Create a local print server
-            LocalPrintServer ps = new LocalPrintServer();
-
-            // List the print server's queues
-            PrintQueueCollection myPrintQueues = ps.GetPrintQueues();
+           string searchQuery = "SELECT * FROM Win32_PrintJob";
+           ManagementObjectSearcher searchPrintJobs = new ManagementObjectSearcher(searchQuery);
            
-            String printQueueNames = "My Print Queues:\n\n";
 
 
-            foreach (PrintQueue pq in myPrintQueues)
+            while (loopIt == true)
             {
-                pq.Refresh();
-                printQueueNames += "\t" + pq.Name + "\n";
-
-                PrintJobInfoCollection jobs = pq.GetPrintJobInfoCollection();
-
-                bool hasJobs = jobs.Any();
-
-                
-
-                foreach (PrintSystemJobInfo job in jobs)
+                ManagementObjectCollection printJobCollection = searchPrintJobs.Get();
+                foreach (ManagementObject manObj in printJobCollection)
                 {
-                    // Since the user may not be able to articulate which job is problematic,
-                    // present information about each job the user has submitted.
-                    
+                    if (printJobCollection.Count != 0 && Convert.ToInt32(manObj.Properties["TotalPages"].Value) != 0 ) 
+                    {
+                        if (jobId != Convert.ToInt32(manObj.Properties["JobId"].Value))
+                        {
+                            printInfo += "\n" + "Printer: " + manObj.Properties["Name"].Value.ToString();
+                            printInfo += "\n" + "Document: " + manObj.Properties["Document"].Value.ToString();
+                            printInfo += "\n" + "Id: " + manObj.Properties["JobId"].Value.ToString();
+                            printInfo += "\n" + "TotalPages: " + manObj.Properties["TotalPages"].Value.ToString();
+
+                            Console.Write(printInfo);
+                            jobId = Convert.ToInt32(manObj.Properties["JobId"].Value);
+                        }
+                    }
                 }
+
+                //looking for jobs here , THEY TOOK OUR JAAAABS!!!
             }
-                Console.WriteLine(printQueueNames);
-                // Console.WriteLine(PrintSystemJobInfo.Get(myPrintQueues,1));
-                Console.WriteLine("\nPress Return to continue.");
 
-
-                //foreach (string printer in printers)
-                //{
-                //    Console.WriteLine(Index.ToString() + " = " + printer);
-                //    Index++;
-                //}
-
-                //Console.WriteLine(Environment.NewLine);
-                //Console.WriteLine("Enter Printer Index:");
-                //short printerIndex = Convert.ToInt16(Console.ReadLine());
-                //string selectedPrinter = printers[printerIndex];
-
-                //Console.WriteLine(prt.GetPrintJobStatus(selectedPrinter));
-                //Console.WriteLine();
-
-                //Console.WriteLine("Press any key to exit...");
-            
-            Console.ReadLine();
         }
     }
 }
